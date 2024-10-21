41c2764441ff113b14776be3034ae2a96564c4b7
Alexandre <alexandre.delesse@isen.yncrea.fr>
2024-10-21 16:48:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve les fichiers statiques du build React
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Pour toutes les autres routes, renvoie le fichier index.html de React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
