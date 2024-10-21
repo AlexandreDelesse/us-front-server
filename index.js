c63f15035f962683344e68f756c1ac1afeaea7f9
Alexandre <alexandre.delesse@isen.yncrea.fr>
2024-10-21 19:05:51 +0200

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Pour toutes les autres routes, renvoie le fichier index.html de React
 app.get('*', (req, res) => {
-  res.send('coucou')
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
