58f7f60ed09b648e75088984f2696296127aa02c
Alexandre <alexandre.delesse@isen.yncrea.fr>
2024-10-21 20:30:05 +0200

    test https

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const path = require('path');
+const https = require("https");
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve les fichiers statiques du build React
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
+
+const certOptions = {
+  key: fs.readFileSync("../selfsigned.key"),
+  cert: fs.readFileSync("../selfsigned.crt"),
+};
 
 // Pour toutes les autres routes, renvoie le fichier index.html de React
-app.get('/dev', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.send("Hello https");
 });
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+https
+  .createServer(options, app)
+  .listen(443, () => console.log("Https server running on port 443"));
